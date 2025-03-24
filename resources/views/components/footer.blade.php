1a90d1c62f8012d53b12a32a1625f4d7108ca9f4
50506576W Adrián Ríos Armero <ara130@alu.ua.es>
2025-03-24 13:15:53 +0100

    Utilización de layout para hacer el navbar y el footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+    <img src="../../assets/Logo.jpg" alt="">
+    <a href="">Carta</a>
+    <a href="">Reservas</a>
+    <a href="">Contáctanos</a>
+    <p>&copy; 2025 - Todos los derechos reservados</p>
+</footer>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,10 @@
+<nav>
+    <a href="/home">
+        <img src="../assets/Logo.jpg" alt="logo de la empresa">
+    </a>
+    <a href=""><strong>Hamburguesa del mes</strong></a>
+    <a href=""><strong>Carta</strong></a>
+    <a href=""><strong>Reservas</strong></a>
+    <a href=""><strong>Contactanos</strong></a>
+    <button>Login</button>
+</nav>
